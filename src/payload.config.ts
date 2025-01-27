18c72380b16a2971001c8cd3989586b15dd4fdbf
Philip Zhan <h6zhan@gmail.com>
2025-01-27 12:33:40 -0800

    add MONGODB_URI

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -64,7 +64,7 @@ export default buildConfig({
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
   db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
+    url: process.env.MONGODB_URI || '',
   }),
   collections: [Pages, Posts, Media, Categories, Users, Tours, Destinations, Reviews, TourDates],
   cors: [getServerSideURL()].filter(Boolean),
