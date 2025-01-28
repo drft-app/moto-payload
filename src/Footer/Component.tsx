9e02a2cdd4fd4a22690338602e2ac2db0e84ff74
Philip Zhan <h6zhan@gmail.com>
2025-01-27 21:08:48 -0800

    copyright

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -15,9 +15,14 @@ export async function Footer() {
   return (
     <footer className="mt-auto border-t border-border bg-black text-white">
       <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
-          <Brand className="text-white" />
-        </Link>
+        <div className="flex flex-col gap-4">
+          <Link className="flex items-center" href="/">
+            <Brand className="text-white" />
+          </Link>
+          <p className="text-sm text-gray-400">
+            © {new Date().getFullYear()} All rights reserved.
+          </p>
+        </div>
 
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
           <nav className="flex flex-col md:flex-row gap-4">
